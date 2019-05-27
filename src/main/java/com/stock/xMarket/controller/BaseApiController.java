22397be543bbf879d1af757ad970973f0c5f63d8
DZL <522627142@qq.com>
2019-05-27 10:39:43 +0800

    错误处理模块导入

--- src/main/java/com/stock/xMarket/controller/BaseApiController.java
+++ src/main/java/com/stock/xMarket/controller/BaseApiController.java
@@ -1,12 +1,37 @@
 package com.stock.xMarket.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.stock.xMarket.error.BusinessException;
+import com.stock.xMarket.error.EmBusinessError;
+import com.stock.xMarket.response.CommonReturnType;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @RequestMapping("/api")
 @CrossOrigin(origins = "*", allowCredentials = "true")
 public class BaseApiController {
+
+
+    //定义exceptionhandler解决未被controller层吸收的exception
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public Object handleException(HttpServletRequest request, Exception ex) {
+
+        CommonReturnType commonReturnType = new CommonReturnType();
+        if (ex instanceof BusinessException) {
+            BusinessException businessException = (BusinessException) ex;
+            commonReturnType.setCode(businessException.getErrCode());
+            commonReturnType.setMessage(businessException.getErrMsg());
+        } else {
+            commonReturnType.setCode(EmBusinessError.UNKNOW_ERROR.getErrCode());
+            commonReturnType.setMessage(EmBusinessError.UNKNOW_ERROR.getErrMsg());
+        }
+
+        return commonReturnType;
+    }
+
 
 }
