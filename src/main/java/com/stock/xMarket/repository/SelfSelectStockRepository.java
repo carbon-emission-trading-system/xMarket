88c7da5e3a8ce51eb8396296d9dcf7bca12d0bf3
wjq-hhh <50319421+wjq-hhh@users.noreply.github.com>
2019-06-04 09:02:41 +0800

    s

--- src/main/java/com/stock/xMarket/repository/SelfSelectStockRepository.java
+++ src/main/java/com/stock/xMarket/repository/SelfSelectStockRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.stock.xMarket.model.SelfSelectStock;
+import com.stock.xMarket.model.User;
 
 @Repository
 public interface SelfSelectStockRepository extends JpaRepository<SelfSelectStock,Integer> {
 	
 	List<SelfSelectStock> findByUserId(int userId);
+	
+	SelfSelectStock findByUser_UserIdAndStock_StockId();
 }
