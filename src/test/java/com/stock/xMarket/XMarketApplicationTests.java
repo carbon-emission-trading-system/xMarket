c348046143de1bdc25c5f83536f75b3f675e40a4
lunacy35 <zh648810337@163.com>
2019-06-04 16:59:57 +0800

    Update XMarketApplicationTests.java

--- src/test/java/com/stock/xMarket/XMarketApplicationTests.java
+++ src/test/java/com/stock/xMarket/XMarketApplicationTests.java
@@ -1,9 +1,13 @@
 package com.stock.xMarket;
 
+import java.util.Date;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.alibaba.fastjson.JSON;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +15,10 @@ public class XMarketApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		Date d=new Date();
+	
+		System.out.println("Adsxadaffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+		System.out.println(JSON.toJSONString(d));
 	}
 
 }
